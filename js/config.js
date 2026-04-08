6f4456cdfa2d8186b3d444f59b933b8e723d7692
Margel Castro <margelgabriel@gmail.com>
2026-04-08 12:56:19 -0600

    correction de error de llamada de productos

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzKrfl7_XumwDQCXm2gdEu7GQXFAAsQRg6ISIqw4_wVEs86_2nzz4EyNS1psJHwm3YS6A/exec"
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzKrfl7_XumwDQCXm2gdEu7GQXFAAsQRg6ISIqw4_wVEs86_2nzz4EyNS1psJHwm3YS6A/exec",
 };
